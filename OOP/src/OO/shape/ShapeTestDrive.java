76aec463bbe21ba36f243cbcbf0d393e6514b017
Thach <hoangthachnao@gmail.com>
2022-01-19 13:58:16 +0700

    up code

---
+++ OOP/src/OO/shape/ShapeTestDrive.java
@@ -0,0 +1,12 @@
+package OO.shape;
+
+public class ShapeTestDrive {
+    public static void main(String[] args) {
+        Circle hinhtron = new Circle("hinh tron", "lan", "black");
+        hinhtron.inThongTin();
+        Square hinhvuong = new Square("hình vuong", "truot", "white");
+        hinhvuong.inThongTin();
+        Triangle hinhtamgiac = new Triangle("hinh tam giac", _"truot dai", "orange");
+        hinhtamgiac.inThongTin();
+    }
+}
