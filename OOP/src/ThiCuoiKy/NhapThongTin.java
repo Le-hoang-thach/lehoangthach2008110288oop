a3c0c7b29508194be1659b49d65fe802bed9a533
Thach <hoangthachnao@gmail.com>
2022-04-22 00:53:05 +0700

    UP code Cuoi Ky

--- OOP/src/ThiCuoiKy/NhapThongTin.java
+++ OOP/src/ThiCuoiKy/NhapThongTin.java
@@ -3,15 +3,24 @@ package ThiCuoiKy;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
-public class NhapThongTin   {
-    static  
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-    static HangHoa hangHoa = new HangSanhSu();
+public class NhapThongTin {
+    
+  
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    
     public Scanner input = new Scanner(System.in);
-
-    public HangHoa inputHangHoa() throws ParseException  {
+    private String goodsName;
+    private String commodityCodes;
+    private int quantityInStock ;
+    private Double price;
+    private  int thoiGianBaoHanhTheoThang;
+    private double congSuat;
+    private String nhaSX;
+    public HangHoa inputHangHoa(List<HangHoa> hangHoas)  {
+        
         int choose;
         HangHoa hangHoa = null;
         System.out.println("Please select the goods to import");
@@ -26,13 +35,33 @@ public class NhapThongTin   {
             case 1:
             input.nextLine();
             System.out.println("Enter item code:");
-            String commodityCodes = input.nextLine();
+            commodityCodes = input.nextLine();
+            for (HangHoa hangHoa2 : hangHoas) {
+                if(hangHoa2.getMaHang().equalsIgnoreCase(commodityCodes)){
+                    System.out.println("Item code already exists");
+                    return null;
+                }
+            }
             System.out.println("Enter item name:");
-            String goodsName =input.nextLine();
+            goodsName = input.nextLine();
+           try {
             System.out.println("Enter the price:");
-            Double price = input.nextDouble();
-            System.out.println("Enter inventory quantity:");
-            int quantityInStock = input.nextInt();
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            
             System.out.println("Enter the warehouse date in the format dd-MM-yyyy: ");
             
             String date = input.next();
@@ -42,43 +71,93 @@ public class NhapThongTin   {
                 //Parsing the String
                 date2 = dateFormat.parse(date);
                 input.nextLine();
-                System.out.println("Nhap nha san xuat:");
-                String nhaSX = input.nextLine();
+                System.out.println("Enter manufacturer:");
+                nhaSX = input.nextLine();
                 hangHoa = new HangSanhSu(commodityCodes, goodsName, price, quantityInStock, dateFormat.parse(date), nhaSX);
             } catch (ParseException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-           
+            
             break;
             case 2: 
             input.nextLine();
             System.out.println("Enter item code:");
-            String commodityCodes2 = input.nextLine();
+            commodityCodes = input.nextLine();
+            for (HangHoa hangHoa2 : hangHoas) {
+                if(hangHoa2.getMaHang().equalsIgnoreCase(commodityCodes)){
+                    System.out.println("Item code already exists");
+                    return null;
+                }
+            }
             System.out.println("Enter item name:");
-            String goodsName2 =input.nextLine();
+            goodsName = input.nextLine();
+           try {
             System.out.println("Enter the price:");
-            Double price2 = input.nextDouble();
-            System.out.println("Enter inventory quantity:");
-            int quantityInStock2 = input.nextInt();
-            System.out.println("Enter the number of months of warranty:");
-            int thoiGianBaoHanhTheoThang = input.nextInt();
-            System.out.println("Enter capacity:");
-            double congSuat = input.nextDouble();
-            hangHoa = new HangDienMay(commodityCodes2, goodsName2, price2, quantityInStock2, thoiGianBaoHanhTheoThang, congSuat);
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            try {
+                System.out.println("Enter the number of months of warranty:");
+                thoiGianBaoHanhTheoThang = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            try {
+                System.out.println("Enter capacity:");
+                congSuat = input.nextDouble();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+           
+            hangHoa = new HangDienMay(commodityCodes, goodsName, price, quantityInStock, thoiGianBaoHanhTheoThang, congSuat);
             
             break;
             case 3:
             input.nextLine();
             System.out.println("Enter item code:");
-            String commodityCodes3 = input.nextLine();
+            commodityCodes = input.nextLine();
+            for (HangHoa hangHoa2 : hangHoas) {
+                if(hangHoa2.getMaHang().equalsIgnoreCase(commodityCodes)){
+                    System.out.println("Item code already exists");
+                    return null;
+                }
+            }
             System.out.println("Enter item name:");
-            String goodsName3 =input.nextLine();
+            goodsName = input.nextLine();
+           try {
             System.out.println("Enter the price:");
-            Double price3 = input.nextDouble();
-            System.out.println("Enter inventory quantity:");
-            int quantityInStock3 = input.nextInt();
-            input.nextLine();
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
             System.out.println("Enter production date according to dd-MM-yyyy: ");
             String ngaySX = input.nextLine();
             Date date3 = null;
@@ -98,7 +177,12 @@ public class NhapThongTin   {
                 //TODO: handle exception
                 e.printStackTrace();
             }
-           hangHoa = new HangThucPham(commodityCodes3, goodsName3, price3, quantityInStock3, dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
+            try {
+                hangHoa = new HangThucPham(commodityCodes, goodsName, price, quantityInStock, dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+          
             break;
             case 4: System.out.println("See you!!!"); break;
             default: System.out.println("You made the wrong choice, try again!!!");
@@ -107,6 +191,155 @@ public class NhapThongTin   {
     
             return hangHoa;
        
+    }
+    public HangHoa suaHang(HangHoa hangHoa){
+        commodityCodes = hangHoa.getMaHang();
+        int choose;
+
+        System.out.println("Please select the goods to import");
+        System.out.println("-------------------------------------");
+        System.out.println("1.Crockery goods        ");
+        System.out.println("2.Electrical goods      ");
+        System.out.println("3.Food goods            ");
+        System.out.println("4.Exit");
+        System.out.println("Please choose:");
+        choose = input.nextInt();
+        switch(choose){
+            case 1:
+            input.nextLine();
+            System.out.println("Enter item name:");
+            goodsName = input.nextLine();
+           try {
+            System.out.println("Enter the price:");
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            
+            System.out.println("Enter the warehouse date in the format dd-MM-yyyy: ");
+            
+            String date = input.next();
+           
+            Date date2 = null;
+            try {
+                //Parsing the String
+                date2 = dateFormat.parse(date);
+                input.nextLine();
+                System.out.println("Enter manufacturer:");
+                nhaSX = input.nextLine();
+                hangHoa = new HangSanhSu(commodityCodes, goodsName, price, quantityInStock, dateFormat.parse(date), nhaSX);
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+            break;
+            case 2: 
+           input.nextLine();
+            System.out.println("Enter item name:");
+            goodsName = input.nextLine();
+           try {
+            System.out.println("Enter the price:");
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            try {
+                System.out.println("Enter the number of months of warranty:");
+                thoiGianBaoHanhTheoThang = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            try {
+                System.out.println("Enter capacity:");
+                congSuat = input.nextDouble();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+           
+            hangHoa = new HangDienMay(commodityCodes, goodsName, price, quantityInStock, thoiGianBaoHanhTheoThang, congSuat);
+            
+            break;
+            case 3:
+            input.nextLine();
+            
+            System.out.println("Enter item name:");
+            goodsName = input.nextLine();
+           try {
+            System.out.println("Enter the price:");
+            price = input.nextDouble();
+           } catch (Exception e) {
+               //TODO: handle exception
+               System.out.println("Enter error");
+                System.out.println("Please enter the number");
+           }
+            
+            try {
+                System.out.println("Enter inventory quantity:");
+                quantityInStock = input.nextInt();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Enter error");
+                System.out.println("Please enter the number");
+            }
+            System.out.println("Enter production date according to dd-MM-yyyy: ");
+            String ngaySX = input.nextLine();
+            Date date3 = null;
+            try {
+                //Parsing the String
+                date3 = dateFormat.parse(ngaySX);
+               } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Enter the expiration date by dd-MM-yyyy: ");
+            String ngayHetHan = input.nextLine();
+            try {
+                date2 = dateFormat.parse(ngayHetHan);
+               
+            } catch (Exception e) {
+                //TODO: handle exception
+                e.printStackTrace();
+            }
+            try {
+                hangHoa = new HangThucPham(commodityCodes, goodsName, price, quantityInStock, dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+          
+            break;
+            case 4: System.out.println("See you!!!"); break;
+            default: System.out.println("You made the wrong choice, try again!!!");
+        }
+
+    
+            return hangHoa;
     }
     
     
--- OOP/src/ThiCuoiKy/main.java
+++ OOP/src/ThiCuoiKy/main.java
@@ -2,70 +2,31 @@ package ThiCuoiKy;
 
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Year;
-import java.util.Calendar;
-import java.util.Date;
-
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class main {
-    static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    
     static NhapThongTin nhapThongTin = new NhapThongTin(); 
-    public static void main(String[] args)    {
+    public static void main(String[] args)   {
         
         int choose;
         DanhSachHangHoa danhSachHangHoa = new DanhSachHangHoa();
-        try {
-            String ngayNhapKho = "17-03-2021";
-            String ngaySX = "20-03-2020";
-            String ngayHetHan = "17-03-2012";
         
-            HangHoa hangHoa0 = new HangSanhSu("2002", "chen su", 20_000, 51,dateFormat.parse(ngayNhapKho) , "Le Huynh Phuoc");
-            HangHoa hangHoa1 = new HangDienMay("2001", "bong den",20-000, 2, 12, 34);
-            HangHoa hangHoa2 = new HangThucPham("2005","xuc xich", 30_000, 3, dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
-            HangHoa hangHoa3 = new HangSanhSu("2004", "to su", 40_000, 4,dateFormat.parse(ngayNhapKho), "Le Huynh Phuoc");
-            HangHoa hangHoa4 = new HangDienMay("2003", "den pin mini", 50_000, 5, 12, 34);
-            HangHoa hangHoa5 = new HangThucPham("2006", "thit heo 456gam", 60_000,6, dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
-            HangHoa hangHoa6 = new HangSanhSu("2007", "dia su",20_000, 7, dateFormat.parse(ngayNhapKho),"Le Huynh Phuoc");
-            HangHoa hangHoa7 = new HangDienMay("2008", "but thu dien", 20_000, 8,12, 34);
-            HangHoa hangHoa8 = new HangThucPham("2009", "thit heo 1kg",90_000,9,dateFormat.parse(ngaySX), dateFormat.parse(ngayHetHan));
-            HangHoa hangHoa9 = new HangSanhSu("2010", "binh su", 100_000, 10, dateFormat.parse(ngayNhapKho), "Le Huynh Phuoc");
-            HangHoa hangHoa10 = new HangDienMay("2011", "may quat",110_000, 11, 12, 34);
-            danhSachHangHoa.input(hangHoa0);
-            danhSachHangHoa.input(hangHoa1);
-            danhSachHangHoa.input(hangHoa2);
-            danhSachHangHoa.input(hangHoa3);
-            danhSachHangHoa.input(hangHoa4);
-            danhSachHangHoa.input(hangHoa5);
-            danhSachHangHoa.input(hangHoa6);
-            danhSachHangHoa.input(hangHoa7);
-            danhSachHangHoa.input(hangHoa8);
-            danhSachHangHoa.input(hangHoa9);
-            danhSachHangHoa.input(hangHoa10);
-       
-            } catch (Exception e) {
-                //TODO: handle exception
-                e.printStackTrace();
-            }
-        
+        danhSachHangHoa.duLieuCoSan();
 
         do{
-            System.out.println("      MENU      ");
+            System.out.println("      MENU         ");
             System.out.println("------------------");
             System.out.println("1.Enter information");
-            System.out.println("2.Print the list");
-            System.out.println("3.Find goods ");
-            System.out.println("4.Delete row");
-            System.out.println("5.Sua hang hoa");
-            System.out.println("6.Statistical");
-            System.out.println("7.Sorting goods");
-            System.out.println("8.Save file");
-            System.out.println("9.Exit");
+            System.out.println("2.Print the list   ");
+            System.out.println("3.Find goods       ");
+            System.out.println("4.Delete row       ");
+            System.out.println("5.Edit goods information");
+            System.out.println("6.Statistical      ");
+            System.out.println("7.Sorting goods    ");
+            System.out.println("8.Read file        ");
+            System.out.println("9.Save file        ");
+            System.out.println("10.Merchandise Reviews");
+            System.out.println("0.Exit             ");
             System.out.println("Please choose:");
             
             choose = danhSachHangHoa.input.nextInt();
@@ -73,11 +34,11 @@ public class main {
                 
                 case 1: 
                 try {
-                    HangHoa hangHoa = nhapThongTin.inputHangHoa();
-                    danhSachHangHoa.input(hangHoa);
+                    danhSachHangHoa.input();
+                    
                 } catch (Exception e) {
                     //TODO: handle exception
-                    e.printStackTrace();
+                    System.out.println("Enter error please try again");
                 }
                
                break;
@@ -90,31 +51,22 @@ public class main {
                 case 4: 
                 danhSachHangHoa.delete();
                 break;
-                case 5:
-                danhSachHangHoa.input.nextLine();
-                System.out.println("Enter the code to correct:");
-                String maHang1 = danhSachHangHoa.input.nextLine();
-                danhSachHangHoa.suThongTin(maHang1);
-                System.out.println("Menu sua thong tin:");
-                try {
-                    HangHoa hangHoa3HangHoa = nhapThongTin.inputHangHoa();
-                    danhSachHangHoa.input(hangHoa3HangHoa);
-                } catch (Exception e) {
-                    //TODO: handle exception
-                }
+                case 5: danhSachHangHoa.suaHangHoaTheoMa();
                 
                 break;
                 
                  
                 case 6: danhSachHangHoa.statistical();break;
                 case 7 : danhSachHangHoa.arrangeGoods();break;
-                case 8 :  break;
-               
-                case 9:  System.out.println("Goodbye");break;
+                case 8 : danhSachHangHoa.docFile(); break;
+                case 9 : danhSachHangHoa.ghiFile();break;
+                case 10: danhSachHangHoa.merchandiseReviews();
+                case 0:  System.out.println("Goodbye");break;
+                
                 default: System.out.println("Error please re-enter!!!");break;
             }
-        }while(choose != 9);
-        // nhap
+        }while(choose != 0);
+    
        
       
        
